f0c743a597f055f2ec8b1935b089e1b9526946eb
Nacer Eddine Attar <attar.nacereddine@gmail.com>
2024-02-08 11:04:26 +0100

    faker init + proddetail

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-import App  from "./App";
-import {createRoot} from "react-dom/client";
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-createRoot(document.querySelector("#root")).render(<App />);
+const root = createRoot(document.querySelector('#root'));
+root.render(<App />);
+//App.js
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import ProductDetailPage from './pages/ProductDetailPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<ProductDetailPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
